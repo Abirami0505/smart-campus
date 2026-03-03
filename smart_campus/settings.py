e477220521b3af7f61c078a8242fe1b0b846423a
Abiramisundari <abiramidurai03@gmail.com>
2026-03-04 02:18:14 +0530

    Fixed login URL settings

--- smart_campus/settings.py
+++ smart_campus/settings.py
@@ -140,8 +140,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # -------------------------------------------------
 # LOGIN / LOGOUT REDIRECTS
 # -------------------------------------------------
-LOGIN_REDIRECT_URL = 'dashboard'
-LOGOUT_REDIRECT_URL = 'login'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/dashboard/'
+LOGOUT_REDIRECT_URL = '/login/'
 
 
 # -------------------------------------------------
